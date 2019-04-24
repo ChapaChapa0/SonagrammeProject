59de869395b8b5a46bc8b55dc4c25d171e0bbc27
HRashwan <hatem.rashwan@gmail.com>
2019-04-24 17:24:06 +0200

    1.1

--- Scripts_PYTHON/creation_base_slices.py
+++ Scripts_PYTHON/creation_base_slices.py
@@ -2,29 +2,31 @@
 import cv2                                # state of the art computer vision algorithms library
 import numpy as np                        # fundamental package for scientific computing
 import pyrealsense2 as rs                 # Intel RealSense cross-platform open-source API
-import open3d as op                       # 3d data processing library
 import time
-import pyaudio
-import wave
-import audioop
 import sys
 print("Environment Ready")
 
 # FUNCTIONS
-def calcul_LS(depth0):
+def calcul_LS(depth0, pas, sigma, seuil):
     
-    epsilon = 0.001
-    LS = depth0[0:-1:2][0:-1:2]
-    condition1 = np.where(depth0 > 550)
+    LS = depth0[0:-1:pas][0:-1:pas]
+    condition = np.where(depth0 > seuil)
+    LS[condition] = 0
+    return LS
+
 
 # MAIN
 # Parameters
-len_im = 640
-wid_im = 480
 window_w = [261,390]
 window_h = [101, 480]
+nb_fs = 3
+pas = 2;
+sigma = 1.5;
+seuil = 550;
 
 # Realsense camera parameter
+len_im = 640
+wid_im = 480
 exp = 8000
 gain = 16
 dis_shift = 0;
@@ -52,15 +54,20 @@ config.enable_stream(rs.stream.color, len_im, wid_im, rs.format.rgb8, 30) #1280 
 config.enable_stream(rs.stream.depth, len_im, wid_im, rs.format.z16, 30) #1280 * 720 ou 640 * 480
 profile = pipe.start(config)
 
+# Discard first frames
+for i in range (0,5):
+    frameset = pipe.wait_for_frames()
+
 # LOOP
 condition = True
 iteration = 0
 old_i = 0
 LS_base = []
-while iteration < 5:
+while iteration < 15:
+    
     raw_input("Press enter...")
     
-    for i in range (0,5):
+    for i in range (0,nb_fs):
         frameset = pipe.wait_for_frames()
 
     # Get arrays of color data
@@ -71,11 +78,11 @@ while iteration < 5:
         # Get arrays of depth data
         depth_frame = frameset.get_depth_frame()
         depth = np.asanyarray(depth_frame.get_data()).astype(np.uint16)
-    
+
+        # Compute LS
         depth0 = np.transpose(depth[window_h[0]: window_h[1]])
         depth0 = np.transpose(depth0[window_w[0]:window_w[1]])
-
-        LS = calcul_LS(depth0)
+        LS = calcul_LS(depth0, pas, sigma, seuil)
         LS_base.append(LS)
     
         iteration = iteration + 1
--- Scripts_PYTHON/lecture_sona.py
+++ Scripts_PYTHON/lecture_sona.py
@@ -2,29 +2,47 @@
 import cv2                                # state of the art computer vision algorithms library
 import numpy as np                        # fundamental package for scientific computing
 import pyrealsense2 as rs                 # Intel RealSense cross-platform open-source API
-import open3d as op                       # 3d data processing library
 import time
 import pyaudio
 import wave
 import audioop
 import sys
+import math
+from threading import Thread
 print("Environment Ready")
 
 # FUNCTIONS
-def calcul_LS(depth0):
+def calcul_LS(depth0, pas, sigma, seuil):
     
-    epsilon = 0.001
-    LS = depth0[0:-1:2][0:-1:2]
-    condition1 = np.where(depth0 > 550)
-    
+    LS = depth0[0:-1:pas][0:-1:pas]
+    condition = np.where(depth0 > seuil)
+    LS[condition] = 0
+    return LS
+
+def compare_LS(base_LS, LS0):
+    i_min = 0
+    score_min = math.inf
+    for i in range(1,len(base_LS)):
+        LS = base_LS[i]
+        masque = np.abs(LS - LS0)
+        score = np.mean(masque)
+        if score < score_min:
+            score_min = score
+            i_min = i
+    return [i_min,score_min]
+        
+
 # MAIN
 # Parameters
-len_im = 640
-wid_im = 480
 window_w = [261,390]
 window_h = [101, 480]
+pas = 2;
+sigma = 1.5;
+seuil = 550;
 
 # Realsense camera parameter
+len_im = 640
+wid_im = 480
 exp = 8000
 gain = 16
 dis_shift = 0;
@@ -53,12 +71,18 @@ config.enable_stream(rs.stream.color, len_im, wid_im, rs.format.rgb8, 30) #1280 
 config.enable_stream(rs.stream.depth, len_im, wid_im, rs.format.z16, 30) #1280 * 720 ou 640 * 480
 profile = pipe.start(config)
 
+# Discard first frames
+for i in range (0,5):
+    frameset = pipe.wait_for_frames()
+
 # INIT AUDIO
-# Data flow block
+# Audio path
+audio_path = 'C:\Users\Hatem\Documents\Paul\SonagrammeProject\Scripts_MATLAB\Audio\empreintes_2.wav'
+# Init data flow block
 chunk_size = 1024
 
 # Open the wav file in read-only mode
-wave_file = wave.open('C:\Users\Hatem\Documents\Paul\SonagrammeProject\Scripts_MATLAB\Audio\empreintes_2.wav',"rb")
+wave_file = wave.open(audio_path,"rb")
 p = pyaudio.PyAudio()
 
 # Audio parameters
@@ -66,6 +90,9 @@ file_format = p.get_format_from_width(wave_file.getsampwidth())
 nb_channels = wave_file.getnchannels()
 width = wave_file.getsampwidth()
 framerate = wave_file.getframerate()
+nb_frames = wave_file.getnframes()
+duree_son = float(nb_frames) / float(framerate)
+duree_LS = duree_son / (len(base_LS) - 1)
 
 # Open the data stream
 stream = p.open(format = file_format,
@@ -73,19 +100,20 @@ stream = p.open(format = file_format,
                 rate = framerate,
                 output = True)
 
-# Acceleration parameters
-speed = 1
-new_fr = framerate
-
 # Read data
 data = wave_file.readframes(chunk_size)
 stream.write(data)
 
+# Init acceleration parameters
+speed = 1
+new_fr = framerate
+
 # LOOP
 condition = True
 iteration = 0
-while iteration < 20:
-    raw_input("Press enter...")
+old_i = 0
+while len(data) > 0:
+    start = time.time()
     
     # Get new frames
     frameset = pipe.wait_for_frames()
@@ -99,12 +127,24 @@ while iteration < 20:
         depth_frame = frameset.get_depth_frame()
         depth = np.asanyarray(depth_frame.get_data()).astype(np.uint16)
         
+        # Compute LS
         depth0 = np.transpose(depth[window_h[0]: window_h[1]])
         depth0 = np.transpose(depth0[window_w[0]:window_w[1]])
-
+        LS = calcul_LS(depth0, pas, sigma, seuil)
         
+        # Compare LS with our base of LS
+        res = compare_LS(base_LS,LS)
+        i_min = res[0]
+        score_min = res[1]
+        
+        # Deduce new speed and chunk_size from LS
+        end = time.time()
+        computing_time = start - end
+        
+        
+
         # Play sound according to new speed
-        new_fr = int(round(new_fr / speed))
+        new_fr = int(round(framerate / speed))
         data = wave_file.readframes(chunk_size)
         modified_data = audioop.ratecv(data, width, nb_channels, framerate, new_fr, None)[0]
         stream.write(modified_data)
